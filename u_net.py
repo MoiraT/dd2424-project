35e47c2200d4d2776f709961ab7a14ffd371dda1
Ingrid <ingrid.asenius@gmail.com>
2021-05-11 11:37:07 +0200

    return model

--- u_net.py
+++ u_net.py
@@ -20,25 +20,28 @@ class ExpLayer:
         self.conv1 = Conv2D(filter=64*factor, kernel_size=(3,3), activation=activ_func, padding=same, kernel_initializer=kernel_initializer)(self.drop)
         self.output = Conv2D(filter=64*factor, kernel_size=(3,3), activation=activ_func, padding=same, kernel_initializer=kernel_initializer)(self.conv1)
 
-    def unet(self, input_size = (256, 256, 1)):
-        inputs = Input(input_size)
+def unet(self, input_size = (256, 256, 1)):
+    
+    input_layer = Input(input_size)
 
-        #contracting path
-        layer1 = ContrLayer(inputs, 1, "relu", "he_normal", 0.5)
-        layer2 = ContrLayer(layer1.output, 2, "relu", "he_normal", 0.5)
-        layer3 = ContrLayer(layer2.output, 4, "relu", "he_normal", 0.5)
-        layer4 = ContrLayer(layer3.output, 8, "relu", "he_normal", 0.5)
+    #contracting path
+    layer1 = ContrLayer(inputs, 1, "relu", "he_normal", 0.5)
+    layer2 = ContrLayer(layer1.output, 2, "relu", "he_normal", 0.5)
+    layer3 = ContrLayer(layer2.output, 4, "relu", "he_normal", 0.5)
+    layer4 = ContrLayer(layer3.output, 8, "relu", "he_normal", 0.5)
 
-        #bottom layer
-        layer5_1 = Conv2D(filter=64*8, kernel_size=(3,3), activation="relu", padding=same, kernel_initializer="he_normal")(layer4.output)
-        layer5_2 = Conv2D(filter=64*8, kernel_size=(3,3), activation="relu", padding=same, kernel_initializer="he_normal")(layer5_1)
+    #bottom layer
+    layer5_1 = Conv2D(filter=64*8, kernel_size=(3,3), activation="relu", padding=same, kernel_initializer="he_normal")(layer4.output)
+    layer5_2 = Conv2D(filter=64*8, kernel_size=(3,3), activation="relu", padding=same, kernel_initializer="he_normal")(layer5_1)
 
-        #expansive path
-        layer4 = ExpLayer(layer5_2, layer4.conv2, 8, "relu", "he_normal", 0.5)
-        layer3 = ExpLayer(layer4.output, layer3.conv2, 4, "relu", "he_normal", 0.5)
-        layer2 = ExpLayer(layer3.output, layer2.conv2, 2, "relu", "he_normal", 0.5)
-        layer1 = ExpLayer(layer2.output, layer1.conv2, 1, "relu", "he_normal", 0.5)
-        
-        outputs = Conv2D(filter=1, kernel_size=(1,1), activation="sigmoid", padding=same)(layer1.output)
+    #expansive path
+    layer4 = ExpLayer(layer5_2, layer4.conv2, 8, "relu", "he_normal", 0.5)
+    layer3 = ExpLayer(layer4.output, layer3.conv2, 4, "relu", "he_normal", 0.5)
+    layer2 = ExpLayer(layer3.output, layer2.conv2, 2, "relu", "he_normal", 0.5)
+    layer1 = ExpLayer(layer2.output, layer1.conv2, 1, "relu", "he_normal", 0.5)
+    
+    output_layer = Conv2D(filter=1, kernel_size=(1,1), activation="sigmoid", padding=same)(layer1.output)
 
-        model = Model(input = inputs, output = outputs)
+    model = Model(input = input_layer, output = output_layer)
+
+    return model

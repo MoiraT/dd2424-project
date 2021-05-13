6986f38c76dc229c04e1406c9a8ba831e0a2a835
MoiraT <moira.twengstrom@hotmail.com>
2021-05-13 13:48:03 +0200

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from unet import unet
 
-
+print("yo")
 model = unet((256, 256, 1))
+print("hey")
--- unet.py
+++ unet.py
@@ -7,9 +7,9 @@ class ContrLayer:
 
     def __init__(self, input_layer, factor, activ_func, kernel_initializer, dropout_rate):
         self.conv1 = Conv2D(filter=64*factor, kernel_size=(3, 3), activation=activ_func,
-                            padding=same, kernel_initializer=kernel_initializer)(input_layer)
+                            padding="same", kernel_initializer=kernel_initializer)(input_layer)
         self.conv2 = Conv2D(filter=64*factor, kernel_size=(3, 3), activation=activ_func,
-                            padding=same, kernel_initializer=kernel_initializer)(self.conv_1)
+                            padding="same", kernel_initializer=kernel_initializer)(self.conv_1)
         self.pool = MaxPooling2D(pool_size=(2, 2))(self.conv2)
         self.output = Dropout(dropout_rate)(self.pool)
 
@@ -19,13 +19,13 @@ class ExpLayer:
     def __init__(self, input_layer, concat_layer, factor, activ_func, kernel_initializer, dropout_rate):
 
         self.deconv1 = Conv2DTranspose(filter=64*factor, kernel_size=(3, 3), strides=(
-            2, 2), activation=activ_func, padding=same, kernel_initializer=kernel_initializer)(input_layer)
+            2, 2), activation=activ_func, padding="same", kernel_initializer=kernel_initializer)(input_layer)
         self.concat = concatenate([concat_layer, self.deconv1])
         self.drop = Dropout(dropout_rate)(self.concat)
         self.conv1 = Conv2D(filter=64*factor, kernel_size=(3, 3), activation=activ_func,
-                            padding=same, kernel_initializer=kernel_initializer)(self.drop)
+                            padding="same", kernel_initializer=kernel_initializer)(self.drop)
         self.output = Conv2D(filter=64*factor, kernel_size=(3, 3), activation=activ_func,
-                             padding=same, kernel_initializer=kernel_initializer)(self.conv1)
+                             padding="same", kernel_initializer=kernel_initializer)(self.conv1)
 
 
 def unet(input_size):
@@ -40,9 +40,9 @@ def unet(input_size):
 
     # bottom layer
     layer5_1 = Conv2D(filter=64*8, kernel_size=(3, 3), activation="relu",
-                      padding=same, kernel_initializer="he_normal")(layer4.output)
+                      padding="same", kernel_initializer="he_normal")(layer4.output)
     layer5_2 = Conv2D(filter=64*8, kernel_size=(3, 3), activation="relu",
-                      padding=same, kernel_initializer="he_normal")(layer5_1)
+                      padding="same", kernel_initializer="he_normal")(layer5_1)
 
     # expansive path
     layer4 = ExpLayer(layer5_2, layer4.conv2, 8, "relu", "he_normal", 0.5)
@@ -51,7 +51,7 @@ def unet(input_size):
     layer1 = ExpLayer(layer2.output, layer1.conv2, 1, "relu", "he_normal", 0.5)
 
     output_layer = Conv2D(filter=1, kernel_size=(
-        1, 1), activation="sigmoid", padding=same)(layer1.output)
+        1, 1), activation="sigmoid", padding="same")(layer1.output)
 
     model = Model(input=input_layer, output=output_layer)
 

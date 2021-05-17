3c6f28e8e9be815881e3eb4c0d98a1da79ef5097
MoiraT <moira.twengstrom@hotmail.com>
2021-05-17 18:02:50 +0200

    japp

--- code/main.py
+++ code/main.py
@@ -1,51 +1,72 @@
-#from unet import unet
-from preprocess import *
+# From our files
+from unet import unet
+from plot_stuff import read_nii, plt_acc, plt_loss, view_samples, plt_segmented
 
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import nibabel as nib
-import matplotlib.pyplot as plt
-#import cv2
-#import tensorflow as tf
+# Required libraries
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import cv2
 
-# crop_images(10)
-#model = unet((256, 256, 1))
+# Or else it'll be a warning bonanza
+#TF_CPP_MIN_LOG_LEVEL = 2
 
+# Parameters
+TEST_SIZE = 0.1
+EPOCHS = 2
+STEPS_PER_EPOCH = 1
+img_size = 128
+
+# Load data
 data = pd.read_csv('../input/covid19-ct-scans/metadata.csv')
 data.head()
 
+# View some samples (saved in "kolla.png") as my GUI is being a dick
+view_samples(data)
 
-def read_nii(filepath):
-    '''
-    Reads .nii file and returns pixel array
-    '''
-    ct_scan = nib.load(filepath)
-    array = ct_scan.get_fdata()
-    array = np.rot90(np.array(array))
-    return(array)
+# Data augmentation
+print("Starting augmentation")
+lungs = []
+infections = []
+antal = 1  # len(data)
+for i in range(antal):
+    ct = read_nii(data['ct_scan'][i])
+    infect = read_nii(data['infection_mask'][i])
+    for ii in range(ct.shape[0]):
+        lung_img = cv2.resize(ct[ii], dsize=(
+            img_size, img_size), interpolation=cv2.INTER_AREA).astype('uint8')
+        infec_img = cv2.resize(infect[ii], dsize=(
+            img_size, img_size), interpolation=cv2.INTER_AREA).astype('uint8')
+        lungs.append(lung_img[..., np.newaxis])
+        infections.append(infec_img[..., np.newaxis])
 
+print("Augmentation done")
 
-sample_ct = read_nii(data.loc[1, 'ct_scan'])
-sample_lung = read_nii(data.loc[1, 'lung_mask'])
-sample_infe = read_nii(data.loc[1, 'infection_mask'])
-sample_all = read_nii(data.loc[1, 'lung_and_infection_mask'])
+lungs = np.array(lungs)
+infections = np.array(infections)
+print("Lung array shape: {}, infection array shape: {}".format(
+    lungs.shape, infections.shape))
 
-fig = plt.figure(figsize=(18, 15))
-plt.subplot(1, 4, 1)
-plt.imshow(sample_ct[..., 150], cmap='bone')
-plt.title('Original Image')
+# Split
+lung_train, lung_test, infect_train, infect_test = train_test_split(
+    lungs, infections, test_size=TEST_SIZE)
 
-plt.subplot(1, 4, 2)
-plt.imshow(sample_ct[..., 150], cmap='bone')
-plt.imshow(sample_lung[..., 150], alpha=0.5, cmap='nipy_spectral')
-plt.title('Lung Mask')
+print("Train set size: {}\nTest set size: {}".format(
+    str(lung_train.shape[0]), str(lung_test.shape[0])))
 
-plt.subplot(1, 4, 3)
-plt.imshow(sample_ct[..., 150], cmap='bone')
-plt.imshow(sample_infe[..., 150], alpha=0.5, cmap='nipy_spectral')
-plt.title('Infection Mask')
+# Create and compile U-net
+model = unet((img_size, img_size, 1))
+model.compile(loss="binary_crossentropy",
+              optimizer="adam", metrics=["accuracy"])
+# model.summary()
 
-plt.subplot(1, 4, 4)
-plt.imshow(sample_ct[..., 150], cmap='bone')
-plt.imshow(sample_all[..., 150], alpha=0.5, cmap='nipy_spectral')
-plt.title('Lung and Infection Mask')
+history = model.fit(lung_train, infect_train, EPOCHS, STEPS_PER_EPOCH,
+                    validation_data=(lung_test, infect_test))
+
+# Plot results
+plt_acc(history)
+plt_loss(history)
+
+# Segment new lungs with the trained U-net
+predicted = model.predict(lung_test)
+plt_segmented(lung_test, infect_test, predicted, 3)
---
+++ code/plot_stuff.py
@@ -0,0 +1,86 @@
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+
+
+def read_nii(filepath):
+    '''
+    Reads .nii file and returns pixel array
+    '''
+    ct_scan = nib.load(filepath)
+    array = ct_scan.get_fdata()
+    array = np.rot90(np.array(array))
+    return(array)
+
+
+def view_samples(data):
+    sample_ct = read_nii(data.loc[1, 'ct_scan'])
+    sample_lung = read_nii(data.loc[1, 'lung_mask'])
+    sample_infe = read_nii(data.loc[1, 'infection_mask'])
+    sample_all = read_nii(data.loc[1, 'lung_and_infection_mask'])
+
+    fig = plt.figure(figsize=(18, 15))
+    plt.subplot(1, 4, 1)
+    plt.imshow(sample_ct[..., 150], cmap='bone')
+    plt.title('Original Image')
+
+    plt.subplot(1, 4, 2)
+    plt.imshow(sample_ct[..., 150], cmap='bone')
+    plt.imshow(sample_lung[..., 150], alpha=0.5, cmap='nipy_spectral')
+    plt.title('Lung Mask')
+
+    plt.subplot(1, 4, 3)
+    plt.imshow(sample_ct[..., 150], cmap='bone')
+    plt.imshow(sample_infe[..., 150], alpha=0.5, cmap='nipy_spectral')
+    plt.title('Infection Mask')
+
+    plt.subplot(1, 4, 4)
+    plt.imshow(sample_ct[..., 150], cmap='bone')
+    plt.imshow(sample_all[..., 150], alpha=0.5, cmap='nipy_spectral')
+    plt.title('Lung and Infection Mask')
+
+    plt.savefig("kolla.png")
+    print("Sample picture saved as kolla.png")
+
+
+def plt_acc(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Accuracy vs Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend(['Train', 'Val'], loc='upper left')
+    plt.savefig("Acc_vs_epoch.png")
+    print("Plot of accuracy vs epochs saved as acc_vs_epoch.png")
+
+
+def plt_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Loss vs Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(['Train', 'Val'], loc='upper right')
+    plt.savefig("loss_vs_epoch.png")
+    print("Plot of loss vs epochs saved as loss_vs_epoch.png")
+
+
+def plt_segmented(lung_test, infect_test, predicted, pic_number):
+    fig = plt.figure(figsize=(18, 15))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(lung_test[pic_number][..., 0], cmap='bone')
+    plt.title('original lung')
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(lung_test[pic_number][..., 0], cmap='bone')
+    plt.imshow(infect_test[3][..., 0], alpha=0.5, cmap="nipy_spectral")
+    plt.title('original infection mask')
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(lung_test[pic_number][..., 0], cmap='bone')
+    plt.imshow(predicted[pic_number][..., 0], alpha=0.5, cmap="nipy_spectral")
+    plt.title('predicted infection mask')
+
+    plt.savefig("result.png")
+    print("Segmented images saved as result.png")
